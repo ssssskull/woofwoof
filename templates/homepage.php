411a886237888937f87a01960bab7be79734cda9
Daniel Papp <danipoker1@gmail.com>
2022-05-16 16:35:31 +0200

    Signup done, remember to discuss sizing on h3

--- templates/homepage.php
+++ templates/homepage.php
@@ -5,17 +5,38 @@
 get_header();?>
 <div class="site-content">
     <main class="main">
-        <article class="editorial">
-            <div class="hero">
+        <div class="hero">
+            <div class="hero__wrapper">
                 <h2 class="hero__title">Are you ready for Wood Woods club?</h2>
                 <p class="hero__paragraph">Enjoy benefits, sales, pre-access to new collections and events hosted for our loyal customers</p>
             </div>
-            <div class="signup">
-                <div class="div">
-                    <h3>Sign up now.</h3>
-                </div>
+        </div>
+        <div class="signup">
+            <div class="signup__wrapper">
+                <h3>Sign up now.</h3>
+                <form class="signup__form">
+                    <div class="footer__newsletter-fields">
+                        <div class="footer__newsletter-field">
+                            <label for="name" class="footer__newsletter-label">First name</label>
+                            <input type="text" id="name" name="name" class="footer__newsletter-input" value="">
+                        </div>
+                    </div>
+                    <div class="footer__newsletter-fields">
+                        <div class="footer__newsletter-field">
+                            <label for="loyalty-mail" class="footer__newsletter-label">Email address</label>
+                            <input type="email" id="loyalty-mail" name="email" class="footer__newsletter-input" value="">
+                        </div>
+                    </div>
+                    <div class="footer__newsletter-fields">
+                        <div class="footer__newsletter-field">
+                            <label for="password" class="footer__newsletter-label">Password</label>
+                            <input type="password" id="password" name="password" class="footer__newsletter-input" value="">
+                        </div>
+                    </div>
+                    <button>Sign up</button>
+                </form>
             </div>
-        </article>
+        </div>
 </main>
 </div>
 <?php get_footer(); ?>
