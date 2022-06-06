27b01b7a7400a14eea49a68a54fecebf697d2e15
Daniel Papp <danipoker1@gmail.com>
2022-06-06 21:10:32 +0200

    Added favicon

--- header.php
+++ header.php
@@ -5,6 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title><?php echo the_title();?></title>
         <meta name="description" content="<?php echo get_bloginfo();?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/app/images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/app/images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/app/images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo get_template_directory_uri();?>/app/images/favicon/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/app/images/favicon/safari-pinned-tab.svg" color="#000000">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/app/images/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-config" content="<?php echo get_template_directory_uri();?>/app/images/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
         <?php wp_head();?>
     </head>
 <body>
